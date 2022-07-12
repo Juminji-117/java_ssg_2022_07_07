876a159b86a11005f0cb667bd13c0af9bcf5eca6
Ju, Minji <alswl0w0@gmail.com>
2022-07-12 12:05:41 +0900

    Rq.getPath 메서드 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,25 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void Rq__getIntParam() {
+    public void Rq__getPath() {
         Rq rq = new Rq("삭제?id=1");
 
-        int id = rq.getIntParam("id", 0);
+        String path = rq.getPath();
 
-        assertEquals(1, id);
+        assertEquals("삭제", path);
     }
 
     @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+        int id = rq.getIntParam("id", 0);
+        assertEquals(1, id);
+    }
+    @Test
     public void Rq__getIntParam__2() {
         Rq rq = new Rq("검색?id=10&no=1");
-
         int id = rq.getIntParam("id", 0);
         int no = rq.getIntParam("no", 0);
-
         assertEquals(10, id);
         assertEquals(1, no);
     }
-
     @Test
     public void 테스트_실험() {
         int rs = 10 + 20;
