7c32af935b6f0e7f128096366b76d75792d4852f
Ju, Minji <alswl0w0@gmail.com>
2022-07-13 14:44:02 +0900

    명언 수정 기능 구현 & WiseSayingController 도입

---
+++ src/main/java/com/ll/exam/WiseSayingController.java
@@ -0,0 +1,116 @@
+package com.ll.exam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WiseSayingController {
+    private List<WiseSaying> wiseSayings;
+    private int wiseSayingLastId;
+    private Scanner sc;
+
+    WiseSayingController(Scanner sc) {
+        this.sc = sc;
+        wiseSayings = new ArrayList<>();
+        wiseSayingLastId=0;
+
+    }
+    private WiseSaying findById(int paramId) {
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.id == paramId) {
+                return wiseSaying;
+            }
+        }
+
+        return null;
+    }
+    public void list(Rq rq) {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("--------------------");
+        for (int i = wiseSayings.size() - 1; i >= 0; i--) { // last_index = size - 1
+            WiseSaying wiseSaying_ = wiseSayings.get(i); // wiseSaying_ 객체에 넣어서 출력
+            System.out.printf("%d / %s / %s \n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+        }
+    }
+
+        public void write(Rq rq) {
+
+            // 1.content, author, wiseSayingLastId 처리
+            System.out.printf("명언 : ");
+            String content = sc.nextLine().trim(); // trim()으로 시작과 끝 공백 제거, 중간 공백은 제거 불가
+            System.out.printf("작가 : ");
+            String author = sc.nextLine().trim();
+            int id = ++wiseSayingLastId; // 명언 글 번호 증가
+
+            //2.wiseSaying 객체 생성 및 add
+            WiseSaying wiseSaying = new WiseSaying(id, content, author);
+            wiseSayings.add(wiseSaying);
+
+            //3.해당 id 등록 완료 멘트
+            System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+
+
+        }
+        public void modify(Rq rq) {
+            //1-1.url에 입력된 id 얻기
+            int paramId = rq.getIntParam("id", 0);
+
+            //1-2.url에 입력된 id가 없다면 작업중지
+            if (paramId ==0) {
+                System.out.println("id를 입력해주세요");
+                return;
+            }
+
+            //2-1.url에 입력된 id에 해당하는 명언객체 찾기
+            WiseSaying foundWiseSaying = findById(paramId);
+
+            //2-2.찾지 못했다면 중지
+            if (foundWiseSaying == null) {
+                System.out.printf("%d번 명언은 존재하지 않습니다. \n", paramId);
+                return;
+            }
+
+            //3-1.기존 명언 content와 새로운 명언 content 출력
+            System.out.printf("명언(기존) : %s\n", foundWiseSaying.content);
+            System.out.printf("명언 : ");
+            foundWiseSaying.content = sc.nextLine();
+
+            //3-2.기존 명언 author와 새로운 명언 author 출력
+            System.out.printf("작가(기존) :%s\n",foundWiseSaying.author );
+            System.out.printf("작가 : ");
+            foundWiseSaying.author = sc.nextLine();
+
+            //4.해당id 명언 수정 완료 멘트
+            System.out.printf("%d번 명언이 수정되었습니다.\n", paramId);
+
+
+        }
+        public void remove(Rq rq) {
+            //1-1.URL에 입력된 id 얻기
+            int paramId = rq.getIntParam("id",0);
+
+            //1-2. URL에 입력된 id가 없다면 작업 중지
+            if(paramId == 0) {
+                System.out.println("id를 입력해주세요");
+                return;
+            }
+
+            //2-1.URL에 입력된 id에 해당하는 명언 객체 찾기
+            WiseSaying foundWiseSaying = findById(paramId);
+
+            //2-2.찾지 못했다면 중지
+            if(foundWiseSaying == null) {
+                System.out.printf("%d번 명언은 존재하지 않습니다.\n",paramId);
+                return;
+            }
+
+            //3.입력된 id에 해당하는 명언 객체를 리스트에서 삭제
+            wiseSayings.remove(foundWiseSaying);
+
+            System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+        }
+
+
+
+
+}
